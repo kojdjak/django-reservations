beba3a613f4fbedb777c451567eaec7b56929ded
kojdjak <juraj.kojdjak@gmail.com>
2016-05-31 15:48:30 +0200

    change index view to template

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello World from Reservations!")
+    return render(request, 'reservations/index.html', {})
