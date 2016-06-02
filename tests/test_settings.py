495ac961489dadd923a6e8328559cef507e9ed66
kojdjak <juraj.kojdjak@gmail.com>
2016-06-02 16:02:05 +0200

    sync - tests are failing.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'fake-key'
 
+DEBUG = True
+
 
 INSTALLED_APPS = [
     'tests',
@@ -22,6 +24,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST_NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
---
+++ tests/tests_model.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+
+class IndexViewTests(TestCase):
+    def test_index_view_basic(self):
+        """
+        Basic test index view.
+        """
+        response = self.client.get(reverse('reservations:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Reservationss")
