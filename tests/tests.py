9767ad5c785e80f6ac76b554ad6f150a3da87912
kojdjak <juraj.kojdjak@gmail.com>
2016-06-10 13:45:13 +0200

    tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from reservations.models import Venue, Field, Reservation
 
 
 class IndexViewTests(TestCase):
@@ -8,5 +9,16 @@ class IndexViewTests(TestCase):
         Basic test index view.
         """
         response = self.client.get(reverse('reservations:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Reservations")
+
+
+class FieldViewTests(TestCase):
+    def setUp(self):
+        venue01 = Venue.objects.create(name="VenueFieldTest01")
+        Field.objects.create(name="FieldReservationdTest01", venue=venue01)
+
+    def test_field_detail(self):
+        response = self.client.get(reverse('reservations:field.detail', kwargs={'field_id':1}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Reservations")
