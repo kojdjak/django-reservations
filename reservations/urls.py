7e756a81819a4d0dc66709572d512ea6f21fe988
kojdjak <juraj.kojdjak@gmail.com>
2016-06-10 15:34:05 +0200

    set time of reservation

--- reservations/urls.py
+++ reservations/urls.py
@@ -6,5 +6,5 @@ app_name = 'reservations'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<field_id>[0-9]+)/$', views.field_detail, name='field.detail'),
-    url(r'^(?P<field_id>[0-9]+)/reserve/(?P<time>[0-9]+)$', views.field_reserve, name='field.reserve'),
+    url(r'^(?P<field_id>[0-9]+)/reserve/(?P<reservation_time>[0-9]+)$', views.field_reserve, name='field.reserve'),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Reservation, Field
+from datetime import datetime
 
 
 def index(request):
@@ -10,12 +11,15 @@ def index(request):
 
 
 def field_detail(request, field_id):
-    return render(request, 'reservations/field_detail.html', {'field_id': field_id, 'hours': range(24), })
+    field = Field.objects.get(id=field_id)
+    return render(request, 'reservations/field_detail.html', {'field_id': field_id, 'field': field, 'hours': range(24), })
 
 
-def field_reserve(request, field_id, time):
+def field_reserve(request, field_id, reservation_time):
     field = Field.objects.get(id=field_id)
     user = None if request.user.is_anonymous() else request.user
-    Reservation.objects.create(name="Reservation", field=field, user=user)
+    today = datetime.today()
+    time = datetime(today.year, today.month, today.day, int(reservation_time))
+    Reservation.objects.create(name="Reservation", field=field, user=user, time=time)
     return HttpResponseRedirect(reverse('reservations:field.detail', kwargs={'field_id': field_id}))
 
