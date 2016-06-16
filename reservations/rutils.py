f417c3c18786e5c5be44f745cadc8e557a2fe1bb
kojdjak <juraj.kojdjak@gmail.com>
2016-06-16 15:57:25 +0200

    fix datetime timezone warning

--- reservations/rutils.py
+++ reservations/rutils.py
@@ -15,7 +15,7 @@ class ReservationExist(Exception):
 def createReservation(field_id, reservation_time, user):
     field = Field.objects.get(id=field_id)
     today = timezone.now()
-    time = datetime(today.year, today.month, today.day, int(reservation_time), tzinfo=today.tzinfo)
+    time = timezone.datetime(today.year, today.month, today.day, int(reservation_time), tzinfo=today.tzinfo)
     Reservation.objects.create(name="Reservation", field=field, user=user, time=time)
     pass
 
@@ -23,6 +23,6 @@ def createReservation(field_id, reservation_time, user):
 def getReservations(field_id):
     field = Field.objects.get(id=field_id)
     today = timezone.now()
-    time = datetime(today.year, today.month, today.day, 0)
+    time = timezone.datetime(today.year, today.month, today.day, 0)
     return Reservation.objects.filter(time__range=[time, time+timedelta(days=1)])
 
