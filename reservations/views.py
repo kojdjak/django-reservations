d8f4b7bdc8ccf32591e56bb1dbb8c9d794ed4120
kojdjak <juraj.kojdjak@gmail.com>
2016-06-20 17:04:09 +0200

    prev / next v1

--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.utils import timezone
+from django.utils import timezone, dateparse
 from .models import Reservation, Field
 from reservations import rutils
 from datetime import datetime, timedelta
@@ -19,11 +19,16 @@ def field_detail(request, field_id):
 def field_detail_date(request, field_id, res_date):
     field = Field.objects.get(id=field_id)
     reservations = rutils.get_reservations(field_id, res_date)
+    today = dateparse.parse_date(res_date) if res_date else timezone.now()
+    res_date_previous = timezone.datetime(today.year, today.month, today.day-1, 0).strftime("%Y-%m-%d")
+    res_date_next = timezone.datetime(today.year, today.month, today.day+1, 0).strftime("%Y-%m-%d")
     hours2res = dict.fromkeys(range(24))
     for reservation in reservations:
         hours2res[reservation.time.hour] = reservation
-        print(reservation.name + str(reservation.time.hour) )
-    return render(request, 'reservations/field_detail.html', {'field_id': field_id, 'field': field, 'res_date': res_date, 'hours2res':hours2res })
+        print(reservation.name + str(reservation.time.hour))
+    return render(request, 'reservations/field_detail.html', {'field_id': field_id, 'field': field, 'res_date': res_date,
+                                                              'hours2res':hours2res, 'res_date_previous':res_date_previous,
+                                                              'res_date_next':res_date_next })
 
 
 def field_reserve(request, field_id, reservation_time):
