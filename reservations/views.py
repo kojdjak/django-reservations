5c9ef1b023955f32dfe71481247f5e92aa49d72a
kojdjak <juraj.kojdjak@gmail.com>
2016-06-21 14:19:13 +0200

    move view data to a class

--- reservations/views.py
+++ reservations/views.py
@@ -12,6 +12,14 @@ def index(request):
     return render(request, 'reservations/index.html', {'fields': fields, })
 
 
+class FieldDetailViewModel():
+    def __init__(self, res_date):
+        self.res_date = res_date
+        self.today = dateparse.parse_date(res_date) if res_date else timezone.now()
+        self.res_date_previous = timezone.datetime(self.today.year, self.today.month, self.today.day-1, 0).strftime("%Y-%m-%d")
+        self.res_date_next = timezone.datetime(self.today.year, self.today.month, self.today.day+1, 0).strftime("%Y-%m-%d")
+
+
 def field_detail(request, field_id):
     return field_detail_date(request, field_id, None)
 
@@ -19,16 +27,12 @@ def field_detail(request, field_id):
 def field_detail_date(request, field_id, res_date):
     field = Field.objects.get(id=field_id)
     reservations = rutils.get_reservations(field_id, res_date)
-    today = dateparse.parse_date(res_date) if res_date else timezone.now()
-    res_date_previous = timezone.datetime(today.year, today.month, today.day-1, 0).strftime("%Y-%m-%d")
-    res_date_next = timezone.datetime(today.year, today.month, today.day+1, 0).strftime("%Y-%m-%d")
+    fdvm = FieldDetailViewModel(res_date)
     hours2res = dict.fromkeys(range(24))
     for reservation in reservations:
         hours2res[reservation.time.hour] = reservation
-        print(reservation.name + str(reservation.time.hour))
     return render(request, 'reservations/field_detail.html', {'field_id': field_id, 'field': field, 'res_date': res_date,
-                                                              'hours2res':hours2res, 'res_date_previous':res_date_previous,
-                                                              'res_date_next':res_date_next })
+                                                              'hours2res':hours2res, 'fdvm':fdvm })
 
 
 def field_reserve(request, field_id, reservation_time):
